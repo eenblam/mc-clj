3c0c6432e7aab473804fb6a9f09376fde678b8e9
ben <ben.a.elam@gmail.com>
2016-04-08 09:31:29 -0500

    Add simulation functions and tests.
    Primary package functionality complete.
    TODO:
    - Create an intermediate data structure for cumulative probability
      matrix. Presently, the cumulative probabilities are computed for each
    iteration. This change would reduce time complexity by doubling spatial
    complexity.
    - Documentation
    - Usage examples

--- src/mc_clj/core.clj
+++ src/mc_clj/core.clj
@@ -1,25 +1,64 @@
 (ns mc-clj.core)
 
-(defn cumsum-base
-  "Basis for the cumsum closure."
+(defn cumprob-base
+  "Basis for the cumprob closure."
   [acc front back]
   (let [new-acc (+ acc (if (empty? back) 0 (first back)))]
     (if (empty? back)
       front
       (recur new-acc
-             (concat front [new-acc])
+             (conj front
+                   ;; 0 if 0, new-acc otherwise
+                   (if (= acc new-acc)
+                     0
+                     new-acc))
              (rest back)))))
 
-(defn cumsum
-  "Return a list providing the cumulative sums of the input collection."
+(defn cumprob
+  "Return a list providing the cumulative sums of positive probabilities in coll."
   [coll]
-  (cumsum-base 0 [] coll))
+  (cumprob-base 0 [] coll))
 
-(defn transpose
-  "Transpose an m-by-n vector of vectors."
-  [])
+(defn accessible
+  "Filters out inaccessible nodes from route mapping."
+  [mapping]
+  (into {} (filter #(> (second %) 0) mapping)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn next-state-base
+  "Basis for the next-state closure."
+  [p cumprobs index]
+  (if (<= p (first cumprobs))
+    index
+    (recur p (rest cumprobs) (inc index))))
+
+(defn next-state-of-sums
+  "Get next state from cumprobs collection."
+  [p cumprobs]
+  (next-state-base p cumprobs 0))
+
+(defn next-state
+  "Get next state from transition matrix row."
+  [row]
+  (next-state-of-sums (rand) (cumprob row)))
+
+(defn trans
+  "Simulate a single transition from state i given transition matrix P."
+  [P i]
+  (next-state (get P i)))
+
+(defn simulate-base
+  "Basis for simulate clojure."
+  [P i n coll]
+  (if (<= n 0)
+    (conj coll i)
+    (recur P (trans P i) (dec n) (conj coll i))))
+
+(defn simulate
+  "Simulate n transitions of Markov chain given by P, beginning in state i."
+  [P i n]
+  (simulate-base P i n []))
+
+(defn simulate-rand
+  "Simulate n transitions of Markov chain given by P, beginning in a random state."
+  [P n]
+  (simulate P (rand-int (count P)) n))
--- test/mc_clj/core_test.clj
+++ test/mc_clj/core_test.clj
@@ -2,14 +2,33 @@
   (:require [clojure.test :refer :all]
             [mc-clj.core :refer :all]))
 
-(deftest cumsum-test
-  (testing "Cumsum"
-    (testing "on its happy path"
-      (is (= '(1 3 6) (cumsum [1 2 3]))))
-    (testing "with negative numbers"
-      (is (= '(1 -1 2) (cumsum [1 -2 3])))))
+(deftest cumprob-test
+  (testing "cumprob"
+    (testing "on positive integers"
+      (is (= [1 3 6] (cumprob [1 2 3]))))
+    (testing "with negative integers"
+      (is (= [1 -1 2] (cumprob [1 -2 3])))))
     (testing "with lists instead of vectors"
-      (is (= '(1 3 6) (cumsum '(1 2 3)))))
+      (is (= [1 3 6] (cumprob '(1 2 3)))))
+    (testing "with zero-valued tail"
+      (is (= [1 3 0] (cumprob [1 2 0]))))
     (testing "failing with bad arguments"
-      (is (thrown? IllegalArgumentException (cumsum 1)))
-      (is (thrown? ClassCastException (cumsum "1")))))
+      (is (thrown? IllegalArgumentException (cumprob 1)))
+      (is (thrown? ClassCastException (cumprob "1")))))
+
+(deftest accessible-test
+  (testing "accessible"
+    (testing "on its happy paths"
+      (is (= {:a 0.1, :c 0.9}
+             (accessible {:a 0.1, :b 0, :c 0.9}))))))
+
+(deftest next-state-of-sums-test
+  (testing "next-state-of-sums"
+    (testing "on its happy paths"
+      (is (= 1 (next-state-of-sums 0.5 [0.1 0.5 1.0]))
+          "returns i when p <= i"))))
+
+(deftest simulate-test
+  (testing "simulate"
+    (testing "singleton class captures state"
+      (is (= [0 0 0 0] (simulate [[1 1] [0.1 0.9]] 0 3))))))
